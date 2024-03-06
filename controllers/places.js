9c45be296cf05d01746d42658d356295f87b2bed
Jbunz <jacob652@aol.com>
2024-03-05 18:27:53 -0800

    Fixed error

--- controllers/places.js
+++ controllers/places.js
@@ -1,23 +1,25 @@
-const router = require('express').Router()
+const router = require("express").Router();
 
-  // GET /places
-app.get('/', (req, res) => {
-        let places = [{
-            name: 'Lunas',
-            city: 'Escondido',
-            state: 'CA',
-            cuisines: 'Mexican',
-            pic: 'http://placekitten.com/250/250'
-          }, {
-            name: 'Louis',
-            city: 'San Diego',
-            state: 'CA',
-            cuisines: 'Italian',
-            pic: 'http://placekitten.com/250/250'
-          }]
-    
-    res.render('places/index', { places })
-  })
-  
-module.exports = router
+// GET /places
+router.get("/", (req, res) => {
+  let places = [
+    {
+      name: "Lunas",
+      city: "Escondido",
+      state: "CA",
+      cuisines: "Mexican",
+      pic: "http://placekitten.com/250/250",
+    },
+    {
+      name: "Louis",
+      city: "San Diego",
+      state: "CA",
+      cuisines: "Italian",
+      pic: "http://placekitten.com/250/250",
+    },
+  ];
 
+  res.render("places/index", { places });
+});
+
+module.exports = router;
