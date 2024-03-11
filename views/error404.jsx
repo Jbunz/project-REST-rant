2d8425d5adf916f6a7a370e4ac69bbaa8ae69a6e
Jbunz <jacob652@aol.com>
2024-03-11 09:42:21 -0700

    Show route connected

--- views/error404.jsx
+++ views/error404.jsx
@@ -1,5 +1,6 @@
 const React = require("react");
 const Def = require("./default");
+ 
 
 function error404() {
   return (
@@ -8,7 +9,7 @@ function error404() {
         <h1>404: PAGE NOT FOUND</h1>
         <p>Oops, sorry, we can't find this page!</p>
         <div>
-          <img src="/images/oops.jpg" alt="oops kid" />
+          <img src="/images/oops.jpg" alt="oops kid" className="image-container"/>
           <div>
             "Photo by{" "}
             <a href="https://unsplash.com/@ilumire?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
