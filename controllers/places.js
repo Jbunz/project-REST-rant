2842c598426e6c93b6bc932186a1f2a82c680f8b
Jbunz <jacob652@aol.com>
2024-03-26 10:03:35 -0700

    added commentary

--- controllers/places.js
+++ controllers/places.js
@@ -4,8 +4,10 @@ const db = require('../models')
 
 router.get('/:id', (req, res) => {
   db.Place.findById(req.params.id)
-  .then((places) => {
-      res.render('places/show', { places })
+  .populate('comments')
+  .then(place => {
+      console.log(place.comments)
+      res.render('places/show', { place })
   })
   .catch(err => {
       console.log('err', err)
@@ -13,18 +15,24 @@ router.get('/:id', (req, res) => {
   })
 })
 
-router.post('/', (req, res) => {
-  db.Place.create(req.body)
-  .then(() => {
-      res.redirect('/places')
+router.post('/:id/comment', (req, res) => {
+  console.log(req.body)
+  db.Place.findById(req.params.id)
+  .then(place => {
+      db.Comment.create(req.body)
+      .then(comment => {
+          place.comments.push(comment.id)
+          place.save()
+          .then(() => {
+              res.redirect(`/places/${req.params.id}`)
+          })
+      })
+      .catch(err => {
+          res.render('error404')
+      })
   })
   .catch(err => {
-      if (err && err.name == 'ValidationError') {
-          // TODO: Generate error message(s)
-      }
-      else {
-          res.render('error404')
-      }
+      res.render('error404')
   })
 })
 
@@ -51,5 +59,12 @@ router.post('/:id/rant', (req, res) => {
 router.delete('/:id/rant/:rantId', (req, res) => {
     res.send('GET /places/:id/rant/:rantId stub')
 })
+
+router.post('/:id/comment', (req, res) => {
+  console.log(req.body)
+  req.body.rant = req.body.rant ? true : false
+  res.send('GET /places/:id/comment stub')
+})
+
 
 module.exports = router
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,8 +1,26 @@
 const React = require("react");
 const Def = require("../default");
 
-function Show(props) {
-  const { data } = props;
+function Show(data) {
+  let comments = (
+    <h3 className="inactive">
+      No comments yet!
+    </h3>
+  )
+  if (data.place.comments.length) {
+    comments = data.place.comments.map(c => {
+      return (
+        <div className="border">
+          <h2 className="rant">{c.rant ? 'Rant! ðŸ˜¡' : 'Rave! ðŸ˜»'}</h2>
+          <h4>{c.content}</h4>
+          <h3>
+            <stong>- {c.author}</stong>
+          </h3>
+          <h4>Rating: {c.stars}</h4>
+        </div>
+      )
+    })
+  }
 
   return (
     <Def>
@@ -28,6 +46,8 @@ function Show(props) {
             ...
           </div>
         </div>
+        <h2>Comments</h2>
+        {comments}
       </main>
 
       <a href={`/places/${data.id}/edit`} className="btn btn-warning">
